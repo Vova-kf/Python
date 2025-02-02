f1a792fc242b4a47b2b7d018818b905d758f3d3b
Vova-kf <vwilly1507@gmail.com>
2025-02-02 08:56:37 +0200

    Calculator

--- 03/04.py
+++ 03/04.py
@@ -1,24 +1,24 @@
 def calculator():
     number_one = float(input('Перше число: '))
-    variant = input('Яка операція (+, -, *, /): ')
+    arithmetic_sign = input('Яка операція (+, -, *, /): ')
     number_two = float(input('Друге число: '))
 
-    if variant == '+':
-        resultat = number_one + number_two
-    elif variant == '-':
-        resultat = number_one - number_two
-    elif variant == '*':
-        resultat = number_one * number_two
-    elif variant == '/':
+    if arithmetic_sign == '+':
+        result = number_one + number_two
+    elif arithmetic_sign == '-':
+        result = number_one - number_two
+    elif arithmetic_sign == '*':
+        result = number_one * number_two
+    elif arithmetic_sign == '/':
         if number_two == 0:
-            print('Помилка! На нуль ділити не можна!!!')
+            print('Error!!! На нуль ділити не можна!!!')
             return
-        resultat = number_one / number_two
+        result = number_one / number_two
     else:
-        print('Помилка! Невідома операція.')
+        print('Error!!!')
         return
 
-    print('Дорівнює: ' + str(resultat))
+    print('Дорівнює: ' + str(result))
 
 calculator()
 
