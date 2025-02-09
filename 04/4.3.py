4f8b427aa35d595e3808028b6d9269d30a4d3de9
Vova-kf <vwilly1507@gmail.com>
2025-02-09 22:53:55 +0200

    new 6

--- 04/4.3.py
+++ 04/4.3.py
@@ -1,5 +1,5 @@
 import random
 my_list = [random.randint(3,10) for i in range(random.randint(3, 10))]
 print(my_list)
-print( [my_list[0], my_list[3], my_list[-2]])
+print( [my_list[0], my_list[2], my_list[-2]])
 
