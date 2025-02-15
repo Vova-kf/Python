bd4fd62f08e0d93621839e47bc0c518bb75b7706
Vova-kf <vwilly1507@gmail.com>
2025-02-15 15:19:34 +0200

    5.2

---
+++ 05/5.2.py
@@ -0,0 +1,27 @@
+def calculator():
+    while True:
+        number_one = float(input('Перше число: '))
+        arithmetic_sign = input('Яка операція (+, -, *, /): ')
+        number_two = float(input('Друге число: '))
+
+        if arithmetic_sign == '+':
+            result = number_one + number_two
+        elif arithmetic_sign == '-':
+            result = number_one - number_two
+        elif arithmetic_sign == '*':
+            result = number_one * number_two
+        elif arithmetic_sign == '/':
+            if number_two == 0:
+                print('Error!!! На нуль ділити не можна!!!')
+                return
+            result = number_one / number_two
+        else:
+            print('Error!!!')
+            return
+
+        print('Дорівнює: ' + str(result))
+        action = input("Продовжити? (y/n): ")
+        if action == "n":
+            break
+calculator()
+
